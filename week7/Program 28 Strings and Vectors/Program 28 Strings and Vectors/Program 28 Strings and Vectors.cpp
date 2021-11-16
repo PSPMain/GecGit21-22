88afd796a9cfdc28f59de13cbe622e9c9ce0ea50
Joel Smith <91889280+PSPMain@users.noreply.github.com>
2021-11-16 12:55:07 +0000

    Update Program 28 Strings and Vectors.cpp

--- week7/Program 28 Strings and Vectors/Program 28 Strings and Vectors/Program 28 Strings and Vectors.cpp
+++ week7/Program 28 Strings and Vectors/Program 28 Strings and Vectors/Program 28 Strings and Vectors.cpp
@@ -17,5 +17,16 @@ int main()
 	cout << "Which item you wish to keep: Healing potion or Fire Spell" << endl; 
 	getline(cin, answer);
 	inventory[2] = answer;
-
+	int inv = 0;
+	while (inv < 3)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (inventory[i] != "")
+			{
+				inv++;
+			}
+		}
+	}
+	cout << "inventory is full!" << endl;
 }
