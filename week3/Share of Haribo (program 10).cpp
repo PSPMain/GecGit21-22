07c514e3356cfbdbd840e1a52d08938675a20d69
Joel Smith <91889280+PSPMain@users.noreply.github.com>
2021-11-16 11:19:04 +0000

    week 3 push

---
+++ week3/Share of Haribo (program 10).cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int harribo = 40;
+	int student = 14;
+	int answer = harribo / student;
+	int remainder = harribo % student;
+	cout << answer << endl;
+	cout << remainder << endl;
+}
