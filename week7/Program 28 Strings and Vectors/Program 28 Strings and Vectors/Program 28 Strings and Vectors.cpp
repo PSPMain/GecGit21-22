665e7d8869f1e7793647d29536b4e62a131ee4fe
Joel Smith <91889280+PSPMain@users.noreply.github.com>
2021-11-16 14:01:22 +0000

    program 28 complete push

--- week7/Program 28 Strings and Vectors/Program 28 Strings and Vectors/Program 28 Strings and Vectors.cpp
+++ week7/Program 28 Strings and Vectors/Program 28 Strings and Vectors/Program 28 Strings and Vectors.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+	int count;
 	cout << "Type which item you wish to keep: Staff or Knife" << endl;
 	string answer;
 	getline(cin, answer);
@@ -17,6 +18,11 @@ int main()
 	cout << "Which item you wish to keep: Healing potion or Fire Spell" << endl; 
 	getline(cin, answer);
 	inventory[2] = answer;
+	cout << "your inventory holds:" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << inventory[i] << endl;
+	}
 	int inv = 0;
 	while (inv < 3)
 	{
@@ -29,4 +35,43 @@ int main()
 		}
 	}
 	cout << "inventory is full!" << endl;
+	cout << "you have found a magic ring what would yo like to replace in your inventory?" << endl;
+	cout << "choices:" << endl;
+	cout << "1. " << inventory[0] << endl;
+	cout << "2. " << inventory[1] << endl;
+	cout << "3. " << inventory[2] << endl;
+	int ans;
+	cin >> ans;
+	inventory[ans-1].replace(0, 10, "magic ring");
+	cout << "your inventory holds:" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << inventory[i] << endl;
+	}
+	cout << "you have been robbed!!!" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		inventory[i] = "";
+	}
+	bool empty = false;
+	if (empty == false)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			
+			if (inventory[i] == "")
+			{
+				count++;
+			}
+			if (count == 3)
+			{
+				empty = true;
+			}
+		}
+	}
+	else
+	{
+	cout << "your inventory holds:" << endl;
+	cout << "Empty!" << endl;
+	}
 }
