cb471896312ddf8dcd91f94f5eee7239c12a3de6
Joel Smith <91889280+PSPMain@users.noreply.github.com>
2021-11-26 14:19:25 +0000

    Update Program 24 Matching Pairs.cpp

--- week6/Program 24 Matching Pairs/Program 24 Matching Pairs/Program 24 Matching Pairs.cpp
+++ week6/Program 24 Matching Pairs/Program 24 Matching Pairs/Program 24 Matching Pairs.cpp
@@ -1,57 +1,108 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
+char boardCards[10] = { 'a', 'e','d','a','b','b','c','d','e','c' };
+bool foundCards[10] = { false };
+int attemptCount = 0;
 
-char cards[10] = { 'A','D','B','A','C','C','D','B','E','E' };
-//cards[10][1]={'1','2','3','4','5','6','7','8','9','10'}
-bool cardsFound[10] = { false };
-int attempts;
-
-
-void gameBoard()
+void drawBoard(int guess1, int guess2)
 {
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "[" << i + 1 << "]";
-	}
-	cout << endl;
-	for (int j = 5; j < 10; j++)
-	{
-		cout << "[" << j + 1 << "]";
-	}
+    system("cls");
+    cout << "the attempt count is: " << attemptCount << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        if (foundCards[i] || i + 1 == guess1 || i + 1 == guess2)
+        {
+            cout << "[" << boardCards[i] << "]";
+        }
+        else
+        {
+            cout << "[" << i + 1 << "]";
+        }
+        if (i == 4)
+        {
+            cout << endl;
+        }
+        
+    }
+    cout << endl;
 }
 
-void GameBoard2(char choice1, char choice2)
+void DrawBoard()
 {
-	cout << cards[choice1] << endl;
-	/*if (choice1 < 6)
-	{
-		for (int i = 0; i < choice1; i++)
-		{
-			cout << "[" << i + 1 << "]";
-		}
-		cout << "[" << cards[choice1] << "]";
-		
-	}*/
-	
+    system("cls");
+    cout << "your current attempt count is: " << attemptCount << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        if (foundCards[i] == true)
+        {
+            cout << "[" << boardCards[i] << "]";
+        }
+        else
+        {
+            cout << "[" << i + 1 << "]";
+        }
+        if (i == 4)
+        {
+            cout << endl;
+        }
+        
+    }
+    cout << endl;
 }
-
 
 int main()
 {
-	int score;
-	bool playing = true;
-	while (playing == true)
-	{
-		gameBoard();
-		char choice1=0;
-		char choice2=0;
-		cout << "pick a card: " << endl;
-		cin >> choice1;
-		system("cls");
-		GameBoard2(choice1, choice2);
-		cin >> choice2;
-	}
-	
+    int score = 0;
+    bool playing = true;
+
+    while (playing == true)
+    {
+        int space;
+        DrawBoard();
+        int guess1=0;
+        int guess2=0;
+        cout << "Pick ur cards" << endl;
+        cin >> guess1;
+        drawBoard(guess1, guess2);
+        cin >> guess2;
+        drawBoard(guess1, guess2);
+        space=getc(stdin);
+        if (boardCards[guess1 - 1] == boardCards[guess2 - 1]&&guess1!=guess2)
+        {
+            cout << "MATCH!!!" << endl;
+            attemptCount++;
+            score++;
+            foundCards[guess1 - 1] = true;
+            foundCards[guess2 - 1] = true;
+            space=getc(stdin);
+        }
+        else
+        {
+            attemptCount++;
+            cout << "no match" << endl;
+            space=getc(stdin);
+        }
+        if (score == 5)
+        {
+            cout << "you WIN!!!!!!!!!!" << endl;
+            cout << "would you like to play again? y/n: " << endl;
+            char num;
+            cin >> num;
+            if (num == 'y')
+            {
+                playing = true;
+                score = 0;
+                attemptCount = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    foundCards[i] = { false };
+                }
+            }
+            else
+            {
+                playing = false;
+            }
+        }
+    }
 }
