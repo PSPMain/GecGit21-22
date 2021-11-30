5f3266fbef42699654d723888940e12457e3dd4d
Joel Smith <91889280+PSPMain@users.noreply.github.com>
2021-11-30 19:58:26 +0000

    Update Program 25 Initials.cpp

--- week7/Program 25 Initials/Program 25 Initials/Program 25 Initials.cpp
+++ week7/Program 25 Initials/Program 25 Initials/Program 25 Initials.cpp
@@ -10,7 +10,7 @@ int main()
 		cin >> initials[i];
 		cout << initials[i] << endl;
 	}
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 7; i++)
 	{
 		cin >> initials[i + 2];
 		cout << initials[i + 2];
