c714fe79deeab4ecee8111c2dca194aa6a95a585
Joel Smith <91889280+PSPMain@users.noreply.github.com>
2022-02-08 11:44:21 +0000

    week 1 2nd tutorial push

--- MarioBaseProject2/MarioBaseProject2/Source.cpp
+++ MarioBaseProject2/MarioBaseProject2/Source.cpp
@@ -1,9 +1,57 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include "constants.h"
+#include <iostream>
+using namespace std;
 
+//globals
+SDL_Window* g_window = nullptr;
+//function prototypes
+bool InitSDL();
+void CloseSDl();
 
 int main(int argc, char* args[])
 {
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+	CloseSDl();
 	return 0;
+}
+
+bool InitSDL()
+{
+	//setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. ERROR: : " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. ERROR: " << SDL_GetError();
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+	IMG_Quit();
+	SDL_Quit();
+}
+
+void CloseSDl()
+{
+
 }
---
+++ MarioBaseProject2/MarioBaseProject2/constants.h
@@ -0,0 +1,5 @@
+#pragma once
+//screen Dimentions
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 416
+
